42485208a194ab2f23aaeaf673d1f4d60b19b714
minhan <alsgks1218@gmail.com>
2019-04-28 17:05:15 +0900

    add pug and layouts

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ import bodyParser from "body-parser";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
-import routes from "./routes";
 
 const app = express();
 
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
