5e78772d3a0a883918fd8f3350864a3644eaa0b1
unknown <alsgks1218@naver.com>
2019-04-30 11:58:24 +0900

    add controllers

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -4,7 +4,8 @@ import { home, search } from "../controllers/videoController";
 import {
 	getJoin,
 	postJoin,
-	login,
+	getLogin,
+	postLogin,
 	logout
 } from "../controllers/userController";
 
@@ -15,7 +16,9 @@ globalRouter.get(routes.home, home);
 globalRouter.get(routes.join, getJoin);
 globalRouter.post(routes.join, postJoin);
 
-globalRouter.get(routes.login, login);
+globalRouter.get(routes.login, getLogin);
+globalRouter.post(routes.login, postLogin);
+
 globalRouter.get(routes.logout, logout);
 globalRouter.get(routes.search, search);
 
