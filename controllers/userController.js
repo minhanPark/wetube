207a66087d8832520ef365155b7c5ed4ae8311e6
unknown <alsgks1218@naver.com>
2019-04-30 09:51:52 +0900

    error fixed and revise join controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,31 @@
-export const join = (req, res) => res.render("join", { pageTitle: "Join" });
+import routes from "../routes";
+
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+
+export const postJoin = (req, res) => {
+	const {
+		body: { name, email, password, password2 }
+	} = req;
+	if (password !== password2) {
+		res.status(400);
+		res.render("join", { pageTitle: "Join" });
+	} else {
+		res.redirect(routes.home);
+	}
+};
 
 export const login = (req, res) => res.render("login", { pageTitle: "Login" });
 
 export const logout = (req, res) =>
-  res.render("logout", { pageTitle: "Logout" });
+	res.render("logout", { pageTitle: "Logout" });
 
 export const users = (req, res) => res.render("users", { pageTitle: "Users" });
 
 export const userDetail = (req, res) =>
-  res.render("userDetail", { pageTitle: "User detail" });
+	res.render("userDetail", { pageTitle: "User detail" });
 
 export const editProfile = (req, res) =>
-  res.render("editProfile", { pageTitle: "Edit profile" });
+	res.render("editProfile", { pageTitle: "Edit profile" });
 
 export const changePassword = (req, res) =>
-  res.render("changePassword", { pageTitle: "Change Password" });
+	res.render("changePassword", { pageTitle: "Change Password" });
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,23 +1,23 @@
 import { videos } from "../db";
 
 export const home = (req, res) =>
-  res.render("home", { pageTitle: "Home", videos });
+	res.render("home", { pageTitle: "Home", videos });
 
 export const search = (req, res) => {
-  const {
-    query: { term: searchingBy }
-  } = req;
-  res.render("search", { pageTitle: "Search", searchingBy });
+	const {
+		query: { term: searchingBy }
+	} = req;
+	res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
 
 export const upload = (req, res) =>
-  res.render("upload", { pageTitle: "Upload" });
+	res.render("upload", { pageTitle: "Upload" });
 
 export const videoDetail = (req, res) =>
-  res.render("videoDetail", { pageTitle: "Video detail" });
+	res.render("videoDetail", { pageTitle: "Video detail" });
 
 export const editVideo = (req, res) =>
-  res.render("editVideo", { pageTitle: "Edit Video" });
+	res.render("editVideo", { pageTitle: "Edit Video" });
 
 export const deleteVideo = (req, res) =>
-  res.render("deleteVideo", { pageTitle: "Delete Video" });
+	res.render("deleteVideo", { pageTitle: "Delete Video" });
