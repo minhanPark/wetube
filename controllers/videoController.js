8b7cfd1453b5a7c8b4571f9f27837092f391d4e8
minhan <alsgks1218@gmail.com>
2019-05-01 23:42:06 +0900

    add mongodb and dotenv

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,30 +1,30 @@
-import { videos } from "../db";
+import routes from "../routes";
 
 export const home = (req, res) =>
-	res.render("home", { pageTitle: "Home", videos });
+  res.render("home", { pageTitle: "Home", videos });
 
 export const search = (req, res) => {
-	const {
-		query: { term: searchingBy }
-	} = req;
-	res.render("search", { pageTitle: "Search", searchingBy, videos });
+  const {
+    query: { term: searchingBy }
+  } = req;
+  res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
 
 export const getUpload = (req, res) =>
-	res.render("upload", { pageTitle: "Upload" });
+  res.render("upload", { pageTitle: "Upload" });
 
 export const postUpload = (req, res) => {
-	const {
-		body: { file, title, description }
-	} = req;
-	res.redirect(routes.videoDetail(324393));
+  const {
+    body: { file, title, description }
+  } = req;
+  res.redirect(routes.videoDetail(324393));
 };
 
 export const videoDetail = (req, res) =>
-	res.render("videoDetail", { pageTitle: "Video detail" });
+  res.render("videoDetail", { pageTitle: "Video detail" });
 
 export const editVideo = (req, res) =>
-	res.render("editVideo", { pageTitle: "Edit Video" });
+  res.render("editVideo", { pageTitle: "Edit Video" });
 
 export const deleteVideo = (req, res) =>
-	res.render("deleteVideo", { pageTitle: "Delete Video" });
+  res.render("deleteVideo", { pageTitle: "Delete Video" });
