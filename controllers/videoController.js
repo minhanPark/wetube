ddb4ea27a37078b42da72ef3ea70421e4d2d4cd6
unknown <alsgks1218@naver.com>
2019-05-16 15:28:29 +0900

    add custom video player

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -51,8 +51,10 @@ export const videoDetail = async (req, res) => {
 		params: { id }
 	} = req;
 	try {
+		console.log("start");
 		const video = await Video.findById(id).populate("creator");
 		console.log(video);
+		console.log(video.creator._id);
 		res.render("videoDetail", { pageTitle: "Video detail", video });
 	} catch (error) {
 		res.redirect(routes.home);
@@ -65,7 +67,11 @@ export const getEditVideo = async (req, res) => {
 	} = req;
 	try {
 		const video = await Video.findById(id);
-		res.render("editVideo", { pageTitle: `Edit ${video.title}`, video });
+		if (video.creator !== req.user.id) {
+			throw Error();
+		} else {
+			res.render("editVideo", { pageTitle: `Edit ${video.title}`, video });
+		}
 	} catch (error) {
 		res.redirect(routes.home);
 	}
@@ -89,7 +95,12 @@ export const deleteVideo = async (req, res) => {
 		params: { id }
 	} = req;
 	try {
-		await Video.findOneAndRemove({ _id: id });
+		const video = await Video.findById(id);
+		if (video.creator !== req.user.id) {
+			throw Error();
+		} else {
+			await Video.findOneAndRemove({ _id: id });
+		}
 	} catch (error) {
 		console.log(error);
 	}
