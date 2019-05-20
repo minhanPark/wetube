5eb1f5a90e232f151c2cf2f1fbcaa73c576581b6
unknown <alsgks1218@naver.com>
2019-05-20 11:57:49 +0900

    add volume controller

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -5,12 +5,15 @@ const volumeBtn = document.getElementById("jsVolumeBtn");
 const fullScrnBtn = document.getElementById("jsFullScreen");
 const currentTime = document.getElementById("currentTime");
 const totalTime = document.getElementById("totalTime");
+const volumeRange = document.getElementById("jsVolume");
 
 function handleVolumeClick() {
 	if (videoPlayer.muted) {
 		videoPlayer.muted = false;
 		volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+		volumeRange.value = videoPlayer.volume;
 	} else {
+		volumeRange.value = 0;
 		videoPlayer.muted = true;
 		volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
 	}
@@ -59,7 +62,7 @@ const formatDate = totalSeconds => {
 	const secondsNumber = parseInt(totalSeconds, 10);
 	let hours = Math.floor(secondsNumber / 3600);
 	let minutes = Math.floor((secondsNumber - hours * 3600) / 60);
-	let seconds = totalSeconds - hours * 3600 - minutes * 60;
+	let seconds = secondsNumber - hours * 3600 - minutes * 60;
 
 	if (hours < 10) {
 		hours = `0${hours}`;
@@ -76,7 +79,7 @@ const formatDate = totalSeconds => {
 };
 
 function getCurrentTime() {
-	currentTime.innerHTML = formatDate(videoPlayer.currentTime);
+	currentTime.innerHTML = formatDate(Math.floor(videoPlayer.currentTime));
 }
 
 function setTotalTime() {
@@ -85,14 +88,35 @@ function setTotalTime() {
 	setInterval(getCurrentTime, 1000);
 }
 
+function handleEnded() {
+	videoPlayer.currentTime = 0;
+	playBtn.innerHTML = '<i class="fas fa-play"></i>';
+}
+
+function handleDrag(event) {
+	const {
+		target: { value }
+	} = event;
+	videoPlayer.volume = value;
+	if (value >= 0.6) {
+		volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+	} else if (value >= 0.2) {
+		volumeBtn.innerHTML = '<i class="fas fa-volume-down"></i>';
+	} else {
+		volumeBtn.innerHTML = '<i class="fas fa-volume-off"></i>';
+	}
+}
+
 function init() {
+	videoPlayer.volume = 0.5;
 	playBtn.addEventListener("click", handlePlayClick);
 	volumeBtn.addEventListener("click", handleVolumeClick);
 	fullScrnBtn.addEventListener("click", goFullScreen);
 	videoPlayer.addEventListener("loadedmetadata", setTotalTime);
+	videoPlayer.addEventListener("ended", handleEnded);
+	volumeRange.addEventListener("input", handleDrag);
 }
 
 if (videoContainer) {
 	init();
-	console.log("start");
 }
